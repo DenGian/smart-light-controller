347e65e5ed7f25118bb56164e60d6d4e0d47c13b
DenGian <mondelaers.ian@gmail.com>
2025-01-03 21:09:17 +0100

    WIP

--- SmartLightSystem/Program.cs
+++ SmartLightSystem/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SmartLightSystem.Controllers;
+using SmartLightSystem.Interfaces;
+using SmartLightSystem.Services;
 
-Console.WriteLine("Hello, World!");
+ITimeProvider timeProvider = new TimeProviderReal();
+ILightElement lightElement = new LightElementStub();
+
+LightController controller = new LightController(timeProvider, lightElement);
+
+while (true)
+{
+    controller.Work();
+    Thread.Sleep(5000);
+}
