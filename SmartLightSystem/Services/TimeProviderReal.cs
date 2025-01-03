0e057ce19b80bf04c83aaf77ecd7ad2e3aaf9235
DenGian <mondelaers.ian@gmail.com>
2025-01-03 21:12:04 +0100

    Add time provider real service

---
+++ SmartLightSystem/Services/TimeProviderReal.cs
@@ -0,0 +1,27 @@
+using SmartLightSystem.Interfaces;
+using SmartLightSystem.Models;
+using Newtonsoft.Json;
+
+namespace SmartLightSystem.Services;
+
+public class TimeProviderReal : ITimeProvider
+{
+    private string _url = "http://worldtimeapi.org/api/timezone/Europe/Brussels";
+
+    public string Url
+    {
+        get => _url;
+        set => _url = value;
+    }
+
+    public DateTime GetCurrentTime()
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var httpResponse = httpClient.GetAsync(_url).GetAwaiter().GetResult();
+            var response = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var timeResponse = JsonConvert.DeserializeObject<TimeResponse>(response);
+            return DateTime.Parse(timeResponse.datetime);
+        }
+    }
+}
