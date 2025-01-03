cf2548ed3e536687a27fb840bf7564275fb066c4
DenGian <mondelaers.ian@gmail.com>
2025-01-03 21:11:22 +0100

    Add Light Element service

---
+++ SmartLightSystem/Services/LightElement.cs
@@ -0,0 +1,21 @@
+using SmartLightSystem.Interfaces;
+
+namespace SmartLightSystem.Services;
+
+public class LightElement : ILightElement
+{
+    public bool IsEnabled
+    {
+        get { throw new NotImplementedException(); }
+    }
+
+    public void Disable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Enable()
+    {
+        throw new NotImplementedException();
+    }
+}
