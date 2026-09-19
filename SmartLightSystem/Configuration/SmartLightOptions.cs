95eeff0c4d4cb7cfc7e189addb398705bf3b10a9
DenGian <mondelaers.ian@gmail.com>
2026-09-19 11:48:06 +0200

    feat(config): add typed controller settings

---
+++ SmartLightSystem/Configuration/SmartLightOptions.cs
@@ -0,0 +1,20 @@
+namespace SmartLightSystem.Configuration;
+
+public sealed class SmartLightOptions
+{
+    public const string SectionName = "SmartLight";
+
+    public TimeOnly ActiveStart { get; init; } = new(20, 0);
+
+    public TimeOnly ActiveEnd { get; init; } = new(6, 0);
+
+    public int MaxConsecutiveFailures { get; init; } = 3;
+
+    public TimeSpan PollingInterval { get; init; } = TimeSpan.FromSeconds(5);
+
+    public string TimeZoneId { get; init; } = "Europe/Brussels";
+
+    public Uri TimeServiceBaseUri { get; init; } = new("https://worldtimeapi.org/api/timezone/");
+
+    public TimeSpan HttpTimeout { get; init; } = TimeSpan.FromSeconds(5);
+}
